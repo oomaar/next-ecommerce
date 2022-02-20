e57de6fca28d1d274ce21baacfb4744f0078ef6c
Omar <omaar_5@hotmail.com>
2022-02-20 19:57:28 +0200

    add favicon

--- global/NextHead.js
+++ global/NextHead.js
@@ -1,17 +1,24 @@
 import Head from "next/head";
 
 export const NextHead = ({ title }) => {
-    return (
-        <Head>
-            <title>{title}</title>
+  return (
+    <Head>
+      <title>{title}</title>
 
-            {/* Google Fonts */}
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
-                rel="stylesheet"
-            />
-        </Head>
-    );
+      {/* Favicon */}
+      <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+
+      {/* Google Fonts */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+  );
 };
