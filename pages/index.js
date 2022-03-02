fc8c7916d1f29aebb8ef9290f09b38ea020d9f79
Omar <omaar_5@hotmail.com>
2022-03-02 21:19:32 +0200

    fetch data on the client side

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import { HomeContainer } from "../containers";
 import { NextHead } from "../global/NextHead";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      await fetch(`api/products`)
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data);
+          setIsLoading(false);
+        });
+    };
+    fetchData();
+  }, []);
+
+  isLoading && <h1>Loading....</h1>;
+  !data && <h1>No Data Available...</h1>;
+
   return (
     <>
       <NextHead title="E-Commerce - Login" />
